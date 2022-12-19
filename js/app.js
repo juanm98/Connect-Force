e6393523872117aebcb065e67853e34efcfbd8bb
Juan Garcia <juanmgarcia098@gmail.com>
2022-12-19 16:17:42 -0400

    Added checkForWinner

--- js/app.js
+++ js/app.js
@@ -73,18 +73,108 @@ let winningCombos = [
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let board, turn, winner, tie
+let board, playerTurn, winner, tie
 
 
 /*------------------------ Cached Element References ------------------------*/
 const circleEl = document.querySelectorAll(".board div")
-const resultEl = document
+const boardEl = document.querySelector(".board")
+const resultEl = document.getElementById("result")
+const messageEL = document.getElementById("message")
+const resetEl = document.getElementById("#btn")
 /*----------------------------- Event Listeners -----------------------------*/
-circleEl.addEventListener('click', alert)
+
+
 
 
 /*-------------------------------- Functions --------------------------------*/
 
-function checkForWinner () {
+init()
 
+function init() {
+    board = [null, null, null, null, null, null, null, null, null,null, null, null, null, null, null, null, null, null,null, null, null, null, null, null, null, null, null,null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
+    playerTurn = 1
+    winner = false
+    tie = false
+}
+
+function render () {
+
+}
+
+function handleClick () {
+
+}
+
+function checkForWinner () {
+    if (
+    Math.abs(board[0] + board[1] + board[2] + board[3]) === 4 ||
+    Math.abs(board[41] + board[40] + board[39] + board[38]) === 4 ||
+    Math.abs(board[7] + board[8] + board[9] + board[10]) === 4 ||
+    Math.abs(board[34] + board[33] + board[32] + board[31]) === 4 ||
+    Math.abs(board[14] + board[15] + board[16] + board[17]) === 4 ||
+    Math.abs(board[27] + board[26] + board[25] + board[24]) === 4 ||
+    Math.abs(board[21] + board[22] + board[23] + board[24]) === 4 ||
+    Math.abs(board[20] + board[19] + board[18] + board[17]) === 4 ||
+    Math.abs(board[28] + board[29] + board[30] + board[31]) === 4 ||
+    Math.abs(board[13] + board[12] + board[11] + board[10]) === 4 ||
+    Math.abs(board[13] + board[12] + board[11] + board[10]) === 4 ||
+    Math.abs(board[35] + board[36] + board[37] + board[38]) === 4 ||
+    Math.abs(board[13] + board[12] + board[11] + board[10]) === 4 ||
+    Math.abs(board[35] + board[36] + board[37] + board[38]) === 4 ||
+    Math.abs(board[6] + board[5] + board[4] + board[3]) === 4 ||
+    Math.abs(board[0] + board[7] + board[14] + board[21]) === 4 ||
+    Math.abs(board[41] + board[34] + board[27] + board[20]) === 4 ||
+    Math.abs(board[40] + board[33] + board[26] + board[19]) === 4 ||
+    Math.abs(board[2] + board[9] + board[16] + board[23]) === 4 ||
+    Math.abs(board[39] + board[32] + board[25] + board[18]) === 4 ||
+    Math.abs(board[3] + board[10] + board[17] + board[24]) === 4 ||
+    Math.abs(board[38] + board[31] + board[24] + board[17]) === 4 ||
+    Math.abs(board[4] + board[11] + board[18] + board[25]) === 4 ||
+    Math.abs(board[37] + board[30] + board[23] + board[16]) === 4 ||
+    Math.abs(board[36] + board[29] + board[22] + board[15]) === 4 ||
+    Math.abs(board[6] + board[13] + board[20] + board[27]) === 4 ||
+    Math.abs(board[35] + board[28] + board[16] + board[24]) === 4 ||
+    Math.abs(board[41] + board[33] + board[25] + board[17]) === 4 ||
+    Math.abs(board[7] + board[15] + board[23] + board[31]) === 4 ||
+    Math.abs(board[34] + board[26] + board[18] + board[10]) === 4 ||
+    Math.abs(board[14] + board[22] + board[30] + board[38]) === 4 ||
+    Math.abs(board[27] + board[19] + board[11] + board[3]) === 4 ||
+    Math.abs(board[35] + board[29] + board[23] + board[17]) === 4 ||
+    Math.abs(board[28] + board[22] + board[16] + board[10]) === 4 ||
+    Math.abs(board[13] + board[19] + board[25] + board[31]) === 4 ||
+    Math.abs(board[21] + board[15] + board[9] + board[3]) === 4 ||
+    Math.abs(board[20] + board[26] + board[32] + board[38]) === 4 ||
+    Math.abs(board[36] + board[30] + board[24] + board[18]) === 4 ||
+    Math.abs(board[37] + board[31] + board[25] + board[19]) === 4 ||
+    Math.abs(board[39] + board[31] + board[23] + board[15]) === 4 ||
+    Math.abs(board[40] + board[32] + board[24] + board[16]) === 4 ||
+    Math.abs(board[9] + board[17] + board[25] + board[33]) === 4 ||
+    Math.abs(board[8] + board[16] + board[24] + board[32]) === 4 ||
+    Math.abs(board[11] + board[17] + board[23] + board[29]) === 4 ||
+    Math.abs(board[12] + board[18] + board[24] + board[30]) === 4 ||
+    Math.abs(board[1] + board[2] + board[3] + board[4]) === 4 ||
+    Math.abs(board[5] + board[4] + board[3] + board[2]) === 4 ||
+    Math.abs(board[8] + board[9] + board[10] + board[11]) === 4 ||
+    Math.abs(board[12] + board[11] + board[10] + board[9]) === 4 ||
+    Math.abs(board[15] + board[16] + board[17] + board[18]) === 4 ||
+    Math.abs(board[19] + board[18] + board[17] + board[16]) === 4 ||
+    Math.abs(board[22] + board[23] + board[24] + board[25]) === 4 ||
+    Math.abs(board[26] + board[25] + board[24] + board[23]) === 4 ||
+    Math.abs(board[29] + board[30] + board[31] + board[32]) === 4 ||
+    Math.abs(board[33] + board[32] + board[31] + board[30]) === 4 ||
+    Math.abs(board[36] + board[37] + board[38] + board[39]) === 4 ||
+    Math.abs(board[40] + board[39] + board[38] + board[37]) === 4 ||
+    Math.abs(board[7] + board[14] + board[21] + board[28]) === 4 ||
+    Math.abs(board[8] + board[15] + board[22] + board[29]) === 4 ||
+    Math.abs(board[9] + board[16] + board[23] + board[30]) === 4 ||
+    Math.abs(board[10] + board[17] + board[24] + board[31]) === 4 ||
+    Math.abs(board[11] + board[18] + board[25] + board[32]) === 4 ||
+    Math.abs(board[12] + board[19] + board[26] + board[33]) === 4 ||
+    Math.abs(board[13] + board[20] + board[27] + board[34]) === 4 
+    
+    ) {
+        winner = true
+    }
+    
 }
