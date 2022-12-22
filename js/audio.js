b96bf58ab12f598c81bf9943ff2a1f6d64a58fc3
Juan Garcia <juanmgarcia098@gmail.com>
2022-12-22 10:29:05 -0400

    Complete Audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,5 +1,6 @@
 let laserSound = new Audio('../assets/Blaster.mp3')
 let victorySound = new Audio('../assets/Victory.mp3')
+let resetSound = new Audio('../assets/Reset.wav')
 
 function playLaser() {
     laserSound.volume = 0.25
@@ -9,4 +10,15 @@ function playLaser() {
 function playVictory() {
     victorySound.volume = 0.25
     victorySound.play()
+}
+
+function playReset() {
+    resetSound.volume = 0.25
+    resetSound.play()
+}
+
+export {
+    playLaser,
+    playVictory,
+    playReset;
 }
